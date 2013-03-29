33629524befb4f6c8a627959106e3c4bef2df6a3
UncleBill <billbill290@gmail.com>
2013-03-29 13:04:05 +0800

    fix the last 2 item

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
-    $(".menu_item").each(function(){
+    $(".menu_item").each(function(index){
         $(this).hover(function(){
+            document.title = index;
+            if( index > 4 ){
+                $(this).addClass("last_menu_item");
+            }
             $(this)
                 .find(".pop_menu")
                 .addClass("mouse-in")
-                .stop().fadeIn();
+                .stop().fadeIn(500,function(){
+                $(this).css({'opacity':1});
+                });
         },function(){
             $(this)
                 .find(".pop_menu")
-                .removeClass("mouse-in")
+                .removeClass("mouse-in","last_menu_item")
                 .stop()
                 .fadeOut();
         });

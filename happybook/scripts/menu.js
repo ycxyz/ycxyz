9feaaa1135c0b6d2ab500c5c34efbd4e369fcb7d
yangha <dongyangpipi@163.com>
2013-05-03 11:50:11 +0800

    add module happy book

---
+++ happybook/scripts/menu.js
@@ -0,0 +1,24 @@
+;$(document).ready(function(){
+
+    $(".menu_item").each(function(index){
+        var isOlderIe = $.browser.msie && (parseInt($.browser.version,10) < 9);
+        var speed = isOlderIe ? 0 : 500;
+        $(this).hover(function(){
+            if( index > 4 ){
+                $(this).addClass("last_menu_item");
+            }
+            $(this)
+                .find(".pop_menu")
+                .addClass("mouse-in")
+                .stop().fadeIn(speed,function(){
+                    $(this).css({'opacity':1,'display':'block'});     // besure div show up.
+                });
+        },function(){
+            $(this)
+                .find(".pop_menu")
+                .stop().delay(100).fadeOut(speed/2)
+                .removeClass("mouse-in","last_menu_item");
+        });
+    });
+
+});

7e7a7f3dc3a5364377c2e52ddde385f0d1a0d30d
UncleBill <billbill290@gmail.com>
2013-05-15 14:10:12 +0800

    move topic_before: js complete

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,44 +3,39 @@
     var $scroller = $("#scroller .scroller_inner");
     var $items = $(".scroll_item");
     var $size = $items.size();
-    var $up = $scroller.find(".up");
-    var $down = $scroller.find(".down");
-    var $animateHeight = $items.height();
-    var $upBtn = $("#topic_before .up");
-    var $downBtn = $("#topic_before .down");
+    var $animateWidth = $items.width();
+    var $upBtn = $("#topic_before .scroller-control-left");
+    var $downBtn = $("#topic_before .scroller-control-right");
     var flag = 0;
 
-    var scrollUp = function(){
-        if( flag > $size - 4 ){     // number of showing up item plus 1
+    var scrollLeft = function(){
+        console.log( "left");
+        if( flag > $size - 5 ){     // number of showing up item plus 1
             $upBtn.fadeOut(function(){
                 $downBtn.fadeIn();
             });
             return;
         }
-        $scroller.animate({"margin-top":"-=" + $animateHeight + "px"},500);
+        $scroller.animate({"margin-left":"-=" + $animateWidth + "px"},500);
         flag += 1;
         $downBtn.fadeIn();
         //console.log( flag );
     };
 
-    var scrollDown = function(){
+    var scrollRight = function(){
         if ( flag < 1 ) {
             $downBtn.fadeOut(function(){
                 $upBtn.fadeIn();
             });
             return;
         }
-        $scroller.animate({"margin-top":"+=" + $animateHeight + "px"},500);
+        $scroller.animate({"margin-left":"+=" + $animateWidth + "px"},500);
         $upBtn.fadeIn();
         flag -= 1;
         //console.log( flag );
     };
 
-    $upBtn.bind( "click",scrollUp );
-    $downBtn.bind( "click",scrollDown )
-
-    $down.bind("click",function(){
-        scrollDown();
-    });
+    $upBtn.bind( "click", scrollLeft );
+    $downBtn.bind( "click", scrollRight )
 
 }()); // closure

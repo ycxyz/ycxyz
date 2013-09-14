e07826483735db874a12e60a5c2d9a2a4dc2d11a
UncleBill <billbill290@gmail.com>
2013-09-14 17:21:01 +0800

    implemented items draggable

--- scripts/tags.js
+++ scripts/tags.js
@@ -1,15 +1,16 @@
-$(".board ul").sortable({
-    revert: true
-});
+var zIndex = 999;
 $(".board .tag").draggable({
     containment: ".board",
     scroll: false,
     cursor: "move",
-    revert: "invalid",
+    // revert: "invalid",
     drag: function () {
-        $(this).addClass("dragging")//.animate({"opacity": 0.5}, 100);
+        zIndex += 1;
+        $(this).css({"z-index":zIndex})
+        $(this).addClass("dragging").stop().animate({"opacity": 0.9}, 100);
     },
     stop: function () {
-        $(this).removeClass('dragging')//.animate({"opacity": 1}, 100);
+        $(this).removeClass('dragging').stop().animate({"opacity": 1}, 100)
+        // $(this).animate({'opacity':1}, 100);
     }
 })
